3c313e2b65145c0fa6e3a45348fcd54f592bf094
Elisei-Trifan <slavaberzan@gmail.com>
2024-08-28 15:11:41 +0300

    Урок 4, дз не сделал

--- src/index.js
+++ src/index.js
@@ -1,8 +1,10 @@
-import React from 'react';
+import React, {Component} from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import App from './components/app/app';
+
+
+
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
@@ -11,7 +13,56 @@ root.render(
   </React.StrictMode>
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+
+
+// class WhoAmI extends Component {
+//     constructor (props) {
+//         super (props)
+//         this.state = {
+//             years: 29
+//         }
+//         this.nextYear = this.nextYear.bind(this)
+//         this.prevYear = this.prevYear.bind(this)
+//     }
+//     nextYear () {
+//         // this.state.years++
+//         this.setState(state => ({
+//             years: ++state.years
+//         }))
+//     }
+//     prevYear () {
+//         this.setState (state => ({
+//             years: --state.years
+//         }))
+//     }
+//     render () {
+//         const {name, surname, link} = this.props
+//         const {years} = this.state
+//         return (
+//             <>
+//                 <button onClick={this.nextYear}> ++ </button>
+//                 <button onClick={this.prevYear}> -- </button>
+//                 <h1> My name is {name}, surname - {surname}, years - {years} </h1>
+//                 <a href={link}>My profile</a>
+//             </>
+//         )
+//     }
+
+// }
+
+
+
+
+
+
+// const All = () => {
+//     return (
+//         <>
+//             <WhoAmI name = 'Elisei' surname = 'Trifan' link = 'google.com' />
+//             <WhoAmI name = 'Alina' surname = 'Trifan' link = 'google.com' />
+//             <WhoAmI name = 'Tima' surname = 'Trifan' link = 'google.com' />
+//         </>
+//     )
+// }
+
+// ReactDOM.render(<All/> , document.getElementById('root'))
